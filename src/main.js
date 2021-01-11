9d4aee5e94b35badb4fb25df952bd36de66034ee
Ahmad Eyamin <ahmadeyamin@gmail.com>
2021-01-11 19:58:27 +0600

    redesign

--- src/main.js
+++ src/main.js
@@ -6,12 +6,6 @@ import './assets/css/default.css'
 import './assets/css/style.css'
 
 
-// window.axios = axios
-// db.collection('posts').onSnapshot(snapshot=>{
-//     snapshot.forEach(data=>{
-//         console.log(data.data());
-//     })
-// })
 let app = createApp(App).use(router)
 
 app.mount('#app')
--- src/views/SingleBlog.vue
+++ src/views/SingleBlog.vue
@@ -1,47 +1,88 @@
 <template>
-    <br>
-    <div v-if="blog" class="container">
-
-        <h2 class="text-center">{{blog.title ?? 'Empty'}}</h2>
-        <hr>
-        <img class='container' :src="blog.image" alt="">
-        <hr>
-        <div class="container">
-            <p>
-                {{blog.body??'Empty'}}
-            </p>
+    <section class="single-article-section">
+        <div class="single-article__wrapper">
+            <div class="single-article-body" v-if="blog">
+                <div class="single-article-header">
+                    <div class="container">
+                        <h2>{{ blog.title ?? "Empty" }}</h2>
+                    </div>
+                </div>
+                <div class="container single-article-content">
+                    <div class="single-article-left">
+                        <div>
+                            <img :src="blog.image" :alt="blog.title ?? ''" />
+                        </div>
+                        <div class="single-article-text">
+                            {{ blog.body ?? "Empty" }}
+                        </div>
+                    </div>
+                    <div class="single-article-right">
+                        <div>
+                            <div class="article-right-top">
+                                <h2 class="article-right-title">
+                                    Latest Article
+                                </h2>
+                                <ul>
+                                    <li>
+                                        <br />
+                                        <center>Empty</center>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-else>
+                <h2>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <center>
+                        Loading...
+                    </center>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </h2>
+            </div>
         </div>
-    </div>
-
-    <br>
-    <br>
+    </section>
 </template>
 
 <script>
-import db from '../firebase'
+import db from "../firebase";
 export default {
     name: "SingleBlog",
     data() {
         return {
-            blog:null,
-        }
+            blog: null,
+        };
     },
-    mounted(){
-            db
-            .collection('posts')
-            .where('slug','==',this.$route.params.slug)
+    mounted() {
+        db.collection("posts")
+            .where("slug", "==", this.$route.params.slug)
             .limit(1)
             .get()
-            .then(function(short){
-                short.forEach(data=>{
-                    this.blog = data.data();
-                })
-            //    console.log(blog);
-            }.bind(this))
-    }
-
-}
+            .then(
+                function (short) {
+                    short.forEach((data) => {
+                        this.blog = data.data();
+                    });
+                    //    console.log(blog);
+                }.bind(this)
+            );
+    },
+};
 </script>
 <style>
-
 </style>
